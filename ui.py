34ee4e14ac937fdcebbf03920584cf57cf3eb0b1
Ali Turfah <aturfah@gmail.com>
2022-05-15 15:51:09 -0400

    ADd ListBox

--- ui.py
+++ ui.py
@@ -23,19 +23,24 @@ class Root(Tk):
         ## Get load/save buttons ready
         self.buttonframe = Frame(self)
         self.buttonframe.grid(row=1, column=0, columnspan=2)
-        load_button = ttk.Button(text="Load File", command=self.load_wrapper)
-        load_button.grid(row=1, column=0)
-        save_button = ttk.Button(text="Save File", command=self.save_wrapper)
-        save_button.grid(row=1, column=1)
+        self.load_button = ttk.Button(text="Load File", command=self._load_wrapper)
+        self.load_button.grid(row=1, column=1)
+        self.save_button = ttk.Button(text="Save File", state=DISABLED, command=self._save_wrapper)
+        self.save_button.grid(row=1, column=3)
 
         ## This text determines if loaded
         self.file_loaded = False
         self.loaded_text = StringVar()
         self.loaded_text.set("No File Loaded")
         self.loaded_label = Label(self, textvariable=self.loaded_text)
-        self.loaded_label.grid(row=2, column=0)
+        self.loaded_label.grid(row=2, column=1)
 
-        ## Set the variables
+        ## Get the grimoire listbox
+        self.grimoire_choices = StringVar(value=[])
+        self.grimoire_disp_list = Listbox(self, listvariable=self.grimoire_choices, height=15)
+        self.grimoire_disp_list.grid(row=3, column=1)
+
+        ## Set the Grimoire variables
         self.file_hex = None
         self.grimoire_data = []
         self.name_id_map = uih.name_id_map()
@@ -43,7 +48,7 @@ class Root(Tk):
     def _error_message(self, title, message):
         messagebox.showerror(title, message)
 
-    def load_wrapper(self):
+    def _load_wrapper(self):
         filename = filedialog.askopenfilename(filetypes=[("SAV files", ".sav")])
         if not filename:
             return
@@ -55,10 +60,14 @@ class Root(Tk):
         except Exception:
             self._error_message("Error Loading", "Please verify you're loading the mor1rgame.sav file")
             return
+        
+        ## Get everything functional
+        self.grimoire_choices.set([x for x in range(1, len(self.grimoire_data) + 1)])
+        self.save_button["state"] = NORMAL
 
         print(len(self.grimoire_data), len(self.file_hex))
 
-    def save_wrapper(self):
+    def _save_wrapper(self):
         if not self.file_loaded:
             self._error_message("Error Saving", "Can't save if you haven't loaded...")
             return
