eebf208735faa0e1ee256063483823821897f94c
Ali Turfah <aturfah@gmail.com>
2022-05-15 16:23:26 -0400

    Add way to short-circuit via edit JSONdirectly

--- hex_compare.py
+++ hex_compare.py
@@ -7,4 +7,8 @@ base_grimoires, base_hex = parse_save_file("backups/base")
 with open("base.json", 'w') as out_file:
     json.dump(base_grimoires, out_file, indent=2)
 
+with open("override.json", 'r') as in_file:
+    override_grimoires = json.load(in_file)
+
 write_save_file(base_hex, base_grimoires)
+write_save_file(base_hex, override_grimoires)
