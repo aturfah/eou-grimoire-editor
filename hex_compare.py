7914900ca8dff19c0bfc029e29ba36aaa1aa7639
Ali Turfah <aturfah@gmail.com>
2022-05-16 13:11:31 -0400

    Flow to edit the grimoires before writing

--- hex_compare.py
+++ hex_compare.py
@@ -1,4 +1,4 @@
-from map_helpers import parse_save_file, write_save_file
+from map_helpers import parse_save_file, write_save_file, ascii_to_hex
 
 import json
 
@@ -9,6 +9,14 @@ with open("base.json", 'w') as out_file:
 
 # with open("override.json", 'r') as in_file:
 #     override_grimoires = json.load(in_file)
+
+## Test out the hex writing
+counter = 0
+names = ["Ali", "Doot", "Pew", "Raquna", "Simon", "Ricky", "Jack"]
+for grim_data in base_grimoires:
+    grim_data["name"] = names[counter]
+    grim_data["name_hex"] = ascii_to_hex(names[counter])
+    counter = (counter + 1) % len(names)
 
 write_save_file(base_hex, base_grimoires)
 # write_save_file(base_hex, override_grimoires)
