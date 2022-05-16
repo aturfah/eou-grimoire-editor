ff11b1e3f1ac0eeb4ee2f088f0d2217f7e125d51
Ali Turfah <aturfah@gmail.com>
2022-05-16 17:27:00 -0400

    Cut out duplicated code

--- ui_helpers.py
+++ ui_helpers.py
@@ -8,22 +8,27 @@ def load_wrapper(filename):
     print("Load Wrapper")
     return mh.parse_save_file(filename)
 
-def name_id_map():
-    """Get the reverse for the map from load_skill_ids"""
+def _invert_dictionary(temp):
     output = {}
-    temp = mh.load_skill_ids()
     for key in temp.keys():
         output[temp[key]] = key
 
     return output
+
+
+def name_id_map():
+    """Get the reverse for the map from load_skill_ids"""
+    return _invert_dictionary(mh.load_skill_ids())
+
 
 def class_id_map():
     """Get the reverse for the map from GRIMOIRE_CLASS_MAP"""
-    output = {}
-    temp = mh.GRIMOIRE_CLASS_MAP
-    for key in temp.keys():
-        output[temp[key]] = key
+    return _invert_dictionary(mh.GRIMOIRE_CLASS_MAP)
 
-    return output
+
+def quality_id_map():
+    """Gets the reverse for the map GRIMOIRE_QUALITY_MAP."""
+    return _invert_dictionary(mh.GRIMOIRE_QUALITY_MAP)
+
 
 ascii_to_hex = mh.ascii_to_hex
